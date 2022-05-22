46f2288871554df48c9cd4db9886f79efd21d41c
Akashrajpoots14 <akashrajpoots14@gmail.com>
2022-05-22 09:43:04 +0530

    Updates the Product id from url

--- simple_tracker.py
+++ simple_tracker.py
@@ -1,5 +1,3 @@
-import time
-from typing_extensions import Self
 from amazon_config import (
     get_web_driver_options,
     get_chrome_web_driver,
@@ -12,6 +10,8 @@ from amazon_config import (
     DIRECTORY
 )
 
+from selenium.webdriver.common.keys import Keys 
+import time
 class GenerateReport:
     def __init__(self):
         pass
@@ -32,20 +32,50 @@ class AmazonAPI:
         
         
     def run(self):
-        print("Starting Script......")
-        print(f"Lokking for {self.search_term} products...")
-        links = self.get_product_links()
-        time.sleep(3)
+        print("Starting Script...")
+        print(f"Looking for {self.search_term} products...")
+        links = self.get_products_links()
+        if not links:
+            print("Stopped script.")
+            return
+        print(f"Got {len(links)} links to products...")
+        print("Getting info about products...")
+        products = self.get_products_info(links)
+        print(f"Got info about {len(products)} products...")
+        products = self.get_product_info(links)
         self.driver.quit()
+        
+        
+    def get_products_info(self,links):    
+        asins = self.get_asins(links)
+        
+        
+    def get_asins(self,links):
+        return [self.get_asins(link) for link in links]
+    
+    def get_asin(product_link):
+            return product_link[product_link.find('/dp/') + 4:product_link.find('/ref')]
         
     def get_product_links(self):
         self.driver.get(self.base_url)
         element = self.driver.find_element_by_xpath('//*[@id="twotabsearchtextbox"]')
         element.send_keys(self.search_term)
         element.send_keys(Keys.ENTER)
-        time.sleep(2)
-        
-
+        time.sleep(2)  # wait to load page
+        self.driver.get(f'{self.driver.current_url}{self.price_filter}')
+        print(f"Our url: {self.driver.current_url}")
+        time.sleep(2)  # wait to load page
+        result_list = self.driver.find_elements_by_class_name('s-result-list')
+        links = []
+        try:
+            results = result_list[0].find_elements_by_xpath(
+                "//div/span/div/div/div[2]/div[2]/div/div[1]/div/div/div[1]/h2/a")
+            links = [link.get_attribute('href') for link in results]
+            return links
+        except Exception as e:
+            print("Didn't get any products...")
+            print(e)
+            return links
 
 if __name__ == '__main__':
     print("Hey...")
