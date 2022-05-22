9f50bce4579235dc4dbf61212cf0a5ee11695f59
Akashrajpoots14 <akashrajpoots14@gmail.com>
2022-05-22 21:16:55 +0530

    Final build

--- amazon_config.py
+++ amazon_config.py
@@ -1,10 +1,10 @@
 from selenium import webdriver
 
 DIRECTORY = 'reports'
-NAME = 'PS4'
+NAME = 'iphone'
 CURRENCY = '€'
-MIN_PRICE = '275'
-MAX_PRICE = '650'
+MIN_PRICE = '200'
+MAX_PRICE = '900'
 FILTERS = {
     'min': MIN_PRICE,
     'max': MAX_PRICE
